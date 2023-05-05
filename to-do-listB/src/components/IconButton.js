3a421cec2ba0f276e1dc82bb9ded042f751fede3
Jason <jasonecy@gmail.com>
2023-05-05 22:13:45 +0900

    Add to-do-listB

---
+++ to-do-listB/src/components/IconButton.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { PropTypes } from "prop-types";
+import styled from "styled-components/native";
+import { images } from "../images";
+
+const Icon = styled.Image`
+  tint-color: ${({ theme, completed }) =>
+    completed ? theme.done : theme.text};
+  width: 30px;
+  height: 30px;
+  margin: 10px;
+`;
+
+export const IconButton = ({ type, onPressOut, id, completed }) => {
+  const _onPressOut = () => {
+    onPressOut(id);
+  };
+
+  return (
+    <TouchableOpacity onPressOut={_onPressOut}>
+      <Icon source={type} completed={completed} />
+    </TouchableOpacity>
+  );
+};
+
+IconButton.defaultProps = {
+  onPressOut: () => {},
+};
+
+IconButton.propTypes = {
+  type: PropTypes.oneOf(Object.values(images)).isRequired,
+  onPressOut: PropTypes.func,
+  id: PropTypes.string,
+  completed: PropTypes.bool,
+};
